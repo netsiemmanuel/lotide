9acb7c9757e7b33ded3a3dd5e30fbf6a783c27ec
Netsanet <netsizebene@gmail.com>
2022-07-27 06:59:02 +0000

    editing eqArrays function so that it can work for strings

--- eqArrays.js
+++ eqArrays.js
@@ -1,18 +1,33 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected ){
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
     process.exit();
   }
   console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
-function eqArrays(arr1,arr2){
-  let val1 = arr1.join();
-  let val2 = arr2.join();
-  if (val1 === val2){
-    return true;
+function eqArrays(arr1, arr2) {
+  for (i = 0; i < arr1.length; i++) {
+    for (j = 0; j < arr2.length; j++) {
+    
+      if (typeof arr1[i] !== typeof arr2[j]) {
+        return false;
+        process.exit();
+      }
+    }
   }
-  return false;
-};
+        let val1 = arr1.join();
+        let val2 = arr2.join();
+        if (val1 === val2) {
+          return true;
+        }
+        return false;
+      
+      }
+      
+      
+    
+  
+
 console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
 console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
