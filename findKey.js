115768774ca46a80ecbbf93a3be13474cd2bb088
Netsanet <netsizebene@gmail.com>
2022-07-30 23:44:55 +0000

    creating findKey.js

---
+++ findKey.js
@@ -0,0 +1,23 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    process.exit();
+  }
+  console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+const findKey = function(obj, func) {
+  for( let key in obj) {
+    if (func(obj[key])){
+      return key;
+    }
+  }
+};
+// --------------TEST CODE ------------
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2));
