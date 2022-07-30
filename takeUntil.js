b923748e3cd8c374156b30ad549c542e6a0e3f62
Netsanet <netsizebene@gmail.com>
2022-07-30 23:23:57 +0000

    creating takeUntil function

--- takeUntil.js
+++ takeUntil.js
@@ -1,18 +1,13 @@
-function findI(data,func){
-  func(data[i]);
-for(let i = 0; i < data.length; i++){
-  if(func(data[i]) === true){
-    console.log(i)
-    return i;
-  }
-}
-}
 function takeUntil(data,func){
   let resultArr = []
-  for(let i = 0; i < findI ; i++){
-    resultArr.push(data[i])
+  for(let i = 0; i < data.length; i++){
+    if (!func(data[i])){
+      resultArr.push(data[i]);
+    } else {
+      return resultArr;
     }
   }
+};
 // -------------TEST CODE------------
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
@@ -23,4 +18,3 @@ console.log('---');
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
-console.log(findI)
