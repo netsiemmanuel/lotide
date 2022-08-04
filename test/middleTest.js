0bcb70d6be30cedde994ddaaf604d41edcea0a10
Netsanet <netsizebene@gmail.com>
2022-08-04 04:15:46 +0000

    refactoring test codes for middle and tail to use unit testing by m&c

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,11 @@
-//-----------TEST CODE----------
-const eqArrays = require('../eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
-const middle = require('../middle');
-assertArraysEqual([3], middle([1, 2, 3, 4, 5,])); // => [3])
-assertArraysEqual([2, 3], middle([1, 2, 3, 4])); // => [2, 3]
-assertArraysEqual([3, 4], middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+const assert = require('chai').assert;
+const middle   = require('../middle');
+
+describe("#middle", () => {
+  it ("returns [3] for [1, 2, 3, 4, 5,]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5,]), [3]);
+  });
+  it ("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+})
